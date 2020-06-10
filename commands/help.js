4e7dd74ca1b418f2c323e7abce987e89adf0de24
SuperGamer1337 <felix.b.aronsson@gmail.com>
2020-06-10 16:08:15 +0200

    Fixed help command bug

--- commands/help.js
+++ commands/help.js
@@ -28,7 +28,7 @@ module.exports = {
 
         }
 
-        const name = args[0].toLowerCase();
+        const name = args[1].toLowerCase();
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
         if (!command) {
@@ -40,8 +40,6 @@ module.exports = {
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
         if (command.description) data.push(`**Description:** ${command.description}`);
         if (command.usage) data.push(`**Usage:** horlman please ${command.name} ${command.usage}`);
-
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
         message.channel.send(data, { split: true });
     }
