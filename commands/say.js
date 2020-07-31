ea9a684339d66e2eb3defafcf8810799d4cc48fd
Supergamer1337 <felix.b.aronsson@gmail.com>
2020-07-31 12:57:26 +0200

    Updated say command and added voice command dependencies

--- commands/say.js
+++ commands/say.js
@@ -4,23 +4,20 @@ module.exports = {
     aliases: ['very', 'good', 'please'],
     usage: '[sentence of what to say]',
     execute(message, args) {
-        // Spaghetti and unoptimized code (like for real though, I need to fix this for V1.01)
-        if(args.some(v => v.toLowerCase() == 'garyan')) {
-            message.channel.send('Garyan really needs to stop drinking...')
+        // Check if sentence contains keyword
+        for(let i = 1; i < args.length; i++) {
+            // Make string lowercase
+            let word = args[i].toLowerCase();
 
-        } else if(args.some(v => v.toLowerCase() == 'ahrune')) {
-            message.channel.send('A-harune is a good friend… But he scares me.')
+            // Check if it matches word in list
+            if(word == 'garyan') return message.channel.send('Garyan really needs to stop drinking...');
+            if(word == 'ahrune') return message.channel.send('A-harune is a good friend… But he scares me.');
+            if(word == 'groove') return message.channel.send('That man Groove sure loves beans.');
+            if(word == 'zedric') return message.channel.send('Zedric once snuck up on me when I was taking a bath… Strange man.');
+            if(word == 'tainika') return message.channel.send('PLEASE GOD NO!');
+        }
 
-        } else if(args.some(v => v.toLowerCase() == 'groove')) {
-            message.channel.send('That man Groove sure loves beans.')
-
-        } else if(args.some(v => v.toLowerCase() == 'zedric')) {
-            message.channel.send('Zedric once snuck up on me when I was taking a bath… Strange man.')
-
-        } else if(args.some(v => v.toLowerCase() == 'tainika')) {
-            message.channel.send('PLEASE GOD NO!')
-            
-        } else if(args[1] !== 'it' && args[1]) {
+        if(args[1] !== 'it' && args[1] || args[2]) {
             args.shift();
             const sentence = args.join(' ');
             message.channel.send(`Ok, "${sentence}"`);
