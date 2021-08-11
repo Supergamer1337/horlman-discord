39f0982c0c32165d1b267082a48a1bbb8e056cae
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2021-08-11 16:15:23 +0200

    Changed to pnpm and update insult

--- app.js
+++ app.js
@@ -1,75 +1,78 @@
 // Import and create client
 const fs = require('fs');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents, Collection } = require('discord.js');
 const config = require('./config/config.json');
 
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGES
+	]
+});
+
 // Configuration
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.commands = new Collection();
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
 // Print when bot is online to console
 client.on('ready', () => {
-    console.log('Bot online...');
+	console.log(`Authenticated as ${client.user.tag}`);
 });
 
 // Text Commands
-client.on('message', message => {
-    // If sent by bot or does not contain prefix exit
-    if(!message.content.toLowerCase().startsWith('horlman') && !message.content.toLowerCase().startsWith('please') || message.author.bot) return;
+client.on('messageCreate', message => {
+	// If sent by bot or does not contain prefix exit
+	if (
+		!message.content.toLowerCase().startsWith('horlman') ||
+		message.author.bot
+	)
+		return;
 
-    // Check for Horlman or Please
-    if(message.content.toLowerCase().startsWith('horlman')) {
-        // Prepare horlman prefix
-        var args = message.content.slice('horlman'.length + 1).split(' ');
+	// Check for Horlman or Please
+	if (message.content.toLowerCase().startsWith('horlman')) {
+		// Prepare horlman prefix
+		var args = message.content.slice('horlman'.length + 1).split(' ');
+	}
 
-        if (args[0].toLowerCase() !== 'please') {
-            return message.channel.send('Please learn some goddamn manners! (Use please)');
-        }
-    } else {
-        // Prepare please prefix
-        var args = message.content.slice('please'.length + 1).split(' ');
+	// Make sure that a command has been issued
+	if (args[0] != undefined) {
+		// Make commandName variable
+		const commandName = args[0].toLowerCase();
 
-        if (args[0].toLowerCase() !== 'horlman') {
-            return message.channel.send('You didn\'t say my name.');
-        }
-    }
+		// Make command variable
+		const command =
+			client.commands.get(commandName) ||
+			client.commands.find(
+				cmd => cmd.aliases && cmd.aliases.includes(commandName)
+			);
 
-    // Remove please or horlman as first argument
-    args.shift();
+		// Check if command exists
+		if (!command) return message.reply('that is not something I can do.');
 
-    // Make sure that a command has been issued
-    if(args[0] != undefined) {
-        // Make commandName variable
-        const commandName = args[0].toLowerCase();
+		// Check for individual commands
+		if (command.guildOnly && message.channel.type !== 'text') {
+			return message.reply("I can't do that here in private!");
+		}
 
-        // Make command variable
-        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-        // Check if command exists
-        if(!command) return message.reply('that is not something I can do.');
-
-        // Check for individual commands
-        if (command.guildOnly && message.channel.type !== 'text') {
-            return message.reply('I can\'t do that here in private!');
-        }
-
-        // Try and execute command
-        try {
-            command.execute(message, args);
-        } catch (error) {
-            console.log(error);
-            message.reply('something went wrong when I tried that.');
-        }
-    } else {
-        message.reply('you didn\'t tell me to do anything.');
-    }
+		// Try and execute command
+		try {
+			command.execute(message, args);
+		} catch (error) {
+			console.log(error);
+			message.reply('something went wrong when I tried that.');
+		}
+	} else {
+		message.reply("you didn't tell me to do anything.");
+	}
 });
 
 // Discord bot login token
--- commands/insult.js
+++ commands/insult.js
@@ -1,15 +1,28 @@
 // Insults
-const insults = ['you make Garyan look like me!', 'I’m not angry at you… Just… Disappointed…', 'you are duller than a khajiit high on moon sugar!', 'you are as weak as I am strong!']
+const insults = [
+	'you make Garyan look like me!',
+	'I’m not angry at you… Just… Disappointed…',
+	'you are duller than a khajiit high on moon sugar!',
+	'you are as weak as I am strong!'
+];
 
 // Exported command
 module.exports = {
-    name: 'insult',
-    description: 'Well, an insult.',
-    execute(message, args) {
-        if(args[1]) {
-            message.channel.send(args[1]+', '+insults[Math.floor(Math.random() * insults.length)]);
-        } else {
-            message.reply('you couldn\'t even make me insult someone else.');
-        }
-    }
-}
+	name: 'insult',
+	description: 'Well, an insult.',
+	execute(message, args) {
+		if (args[1]) {
+			message.channel.send(
+				args[1] +
+					', ' +
+					insults[Math.floor(Math.random() * insults.length)]
+			);
+		} else {
+			message.reply(
+				message.author.username +
+					', ' +
+					insults[Math.floor(Math.random() * insults.length)]
+			);
+		}
+	}
+};
