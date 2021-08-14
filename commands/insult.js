3a60f1868421dd77c4011db79fcd6fa2fb60a448
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2021-08-14 20:00:17 +0200

    Added lost insult

--- commands/insult.js
+++ commands/insult.js
@@ -3,7 +3,8 @@ const insults = [
 	'you make Garyan look like me!',
 	'I’m not angry at you… Just… Disappointed…',
 	'you are duller than a khajiit high on moon sugar!',
-	'you are as weak as I am strong!'
+	'you are as weak as I am strong!',
+	"do you even drink milk? I thought you didn't, at least not with those things you call arms!"
 ];
 
 // Exported command
